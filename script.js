71cbc59f1e936c9c9b949fe13d0bb2334350fe69
Harshang Thakar <harshang.developer@gmail.com>
2023-11-06 11:48:28 +0530

    ajax path updated

--- script.js
+++ script.js
@@ -433,7 +433,7 @@ try {
         sending = true;
         contact_form_button.innerHTML = '<img src="images/time.svg" alt="sending"> <span>Sending</span>';
         $.ajax({
-          url: "http://dj.expansers.com/mail.php",
+          url: "https://dj.expansers.com/mail.php",
           type: "post",
           data: {
             name: input_name.value,
